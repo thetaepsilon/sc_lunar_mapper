2fad403050df61041fd383b87a766f801394ae60
thetaepsilon <thetaepsilon@noreply.users.github.com>
2019-05-26 12:28:35 +0100

    [+] add IConfig with exporter from keymap

---
+++ KeymapEmbeddedConfig.lua
@@ -0,0 +1,9 @@
+--[[
+IConfig specified within the keymap itself.
+Used for various misc settings like event passthrough.
+]]
+return function(IKeymap)
+	local IConfig = IKeymap.config
+	return IConfig
+end
+
