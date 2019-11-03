7b3004c3831295ef0dedceb0aec14155d6edb740
thetaepsilon <thetaepsilon@noreply.users.github.com>
2019-11-03 20:26:20 +0000

    [*] main.lua: remove hardcoding of handler script, allow specifying path.

--- main.lua
+++ main.lua
@@ -2,22 +2,26 @@
 
 local evdev = require "evdev"
 
+local script, dev = ...
+if script == nil or dev == nil then
+	error("Usage: mapper path/to/handler_script.lua /dev/input/eventX")
+end
+
+
+
 local loader = function(c)
 	-- TODO: may want to make this runnable outside the source directory.
 	-- obviously this will fail if you're not in the source directory!
 	return dofile("./"..c..".lua")
 end
-local IEventHandler = dofile("./di.lua")(loader)
+
+local IEventHandler = dofile(script)(loader)
 local handle = IEventHandler.process_event
 
 
 
-local dev = ...
-if dev == nil then
-	error("Usage: mapper /dev/input/eventX")
-end
-local input = evdev.Device(dev)
 
+local input = evdev.Device(dev)
 
 
 
--- setup.sh
+++ setup.sh
@@ -1,2 +1,1 @@
 eval $(luarocks path);
-echo "don't forget to set SC_KEYMAP_PATH; have a look in example_keymaps/";
