40cb4e37fe4ecb989fab160a99abde9c7ee0fabe
thetaepsilon <thetaepsilon@noreply.users.github.com>
2020-02-16 20:03:44 +0000

    example keymaps/minetest: tweaks to button and analog handling

--- example_keymaps/minetest.lua
+++ example_keymaps/minetest.lua
@@ -6,19 +6,20 @@ return function(c)
 
 	local next = c.KEY_N
 	local prev = c.KEY_B
-	map(c.BTN_DPAD_LEFT, prev)
-	map(c.BTN_DPAD_RIGHT, next)
-	map(c.BTN_DPAD_UP, next)
-	map(c.BTN_DPAD_DOWN, prev)
+	--map(c.BTN_DPAD_LEFT, prev)
+	--map(c.BTN_DPAD_RIGHT, next)
+	--map(c.BTN_DPAD_UP, next)
+	--map(c.BTN_DPAD_DOWN, prev)
 	-- Y key... why is it west. why.
 	map(c.BTN_WEST, prev)
 	map(c.BTN_B, next)
 	map(c.BTN_GEAR_DOWN, c.KEY_LEFTSHIFT)
 	map(c.BTN_GEAR_UP, c.KEY_SPACE)
-	map(c.BTN_THUMBL, c.KEY_R)
+	--map(c.BTN_THUMBL, c.KEY_R)
 	map(c.BTN_SELECT, c.KEY_ESC)
 	map(c.BTN_START, c.KEY_E)
-	map(c.BTN_TR, c.KEY_Z)
+	map(c.BTN_TR, c.KEY_R)
+	map(c.BTN_THUMBR, c.KEY_Z)
 	map(c.BTN_TL, c.KEY_LEFTCTRL)
 	map(c.BTN_MODE, c.KEY_F12)
 	-- again, weird directions
@@ -33,11 +34,14 @@ return function(c)
 	mouse(c.BTN_TL2, c.BTN_0)
 	mouse(c.BTN_TR2, c.BTN_2)
 
-	local joystick_threshold = 8000
+	local joystick_threshold = 4000
+	local ltouch_threshold = 10000
 	local dpad = {
 		-- why tf is Y axis inverted in hardware!?
 		[c.ABS_Y] = { joystick_threshold, c.KEY_S, c.KEY_W },
 		[c.ABS_X] = { joystick_threshold, c.KEY_D, c.KEY_A },
+		--[c.ABS_HAT0Y] = { ltouch_threshold, c.KEY_S, c.KEY_W },
+		--[c.ABS_HAT0X] = { ltouch_threshold, c.KEY_D, c.KEY_A },
 	}
 
 
